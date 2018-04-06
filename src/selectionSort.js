9c1dd26fe01c71ac0311d769d43d23c20dcb3013
rfarney3 <farneyr@yahoo.com>
2018-04-06 09:46:42 -0400

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,22 @@
-function selectionSort(arr) {
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function selectionSort(array) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for(var i = 0; i < array.length; i++) {
+      var min = i;
+      for(var j = i + 1; j < array.length; j++) {
+        if(array[j] < array[min]) {
+          min = j;
+        }
+      }
+      if(i !== min) {
+        swap(array, i, min);
+      }
+    }
+    return array;
 }
