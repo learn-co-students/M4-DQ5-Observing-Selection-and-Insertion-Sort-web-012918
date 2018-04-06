900d6773e3b24506c45b4f0be5b9b8dc18c072cf
johnvoyage <jmyork762@gmail.com>
2018-04-06 11:35:12 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,18 +1,31 @@
+// function insertionSort(arr) {
+//
+//   let temporaryNum = 0;
+//   for (let i = 0; i < arr.length; ++i) {
+//     for (let j = i + 1; j < arr.length; ++j) {
+//       //if lest side number is greater than RSN
+//       if (arr[i] > arr[j]) {
+//         // console.log(arr)
+//         //set temporary variable equal to LSN
+//         temporaryNum = arr[i];
+//         //make LSN equal to RSN
+//         arr[i] = arr[j];
+//         arr[j] = temporaryNum;
+//         //make RSN equal to LSN
+//       }
+//     }
+//   }
+//   return arr
+// }
+
+
 function insertionSort(arr) {
-  let temporaryNum = 0;
-  for (let i = 0; i < arr.length; ++i) {
-    for (let j = i + 1; j < arr.length; ++j) {
-      //if lest side number is greater than RSN
-      if (arr[i] > arr[j]) {
-        // console.log(arr)
-        //set temporary variable equal to LSN
-        temporaryNum = arr[i];
-        //make LSN equal to RSN
-        arr[i] = arr[j];
-        arr[j] = temporaryNum;
-        //make RSN equal to LSN
-      }
+  let startIdx = 0
+  for (let startIdx = 0; startIdx < arr.length; startIdx++) {
+    for(let currIdx = startIdx; currIdx > 0 && arr[currIdx-1] > arr[currIdx]; currIdx--) {
+      let temp = arr[currIdx]
+      arr[currIdx] = arr[currIdx-1]
+      arr[currIdx-1] = temp
     }
   }
-  return arr
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,23 +1,36 @@
-const findMinimum = (arr) => {
-  let min = arr[0]
-  for (let element of arr) {
-    if (element < min) {
-      min = element
-    }
-  }
-  return min;
-}
+// const findMinimum = (arr) => {
+//   let min = arr[0]
+//   for (let element of arr) {
+//     if (element < min) {
+//       min = element
+//     }
+//   }
+//   return min;
+// }
+//
+// function selectionSort(arr) {
+//   let sortedArray = [];
+//   let counterMax = arr.length
+//   let counter = 0
+//   while (counter < counterMax) {
+//     currentMinimum = findMinimum(arr)
+//     sortedArray.push(currentMinimum)
+//     minimumIndex = arr.indexOf(currentMinimum)
+//     arr.splice(minimumIndex, 1)
+//     counter++
+//   }
+//   return sortedArray
+// }
 
 function selectionSort(arr) {
-  let sortedArray = [];
-  let counterMax = arr.length
-  let counter = 0
-  while (counter < counterMax) {
-    currentMinimum = findMinimum(arr)
-    sortedArray.push(currentMinimum)
-    minimumIndex = arr.indexOf(currentMinimum)
-    arr.splice(minimumIndex, 1)
-    counter++
+  for (let oIdx = 0; oIdx < arr.length-1; oIdx++) {
+    let minIdx = oIdx
+    for (var iIdx = oIdx; iIdx < arr.length; iIdx++) {
+      if (arr[iIdx] < arr[minIdx])
+        minIdx = iIdx
+    }
+    let temp = arr[oIdx]
+    arr[oIdx] = arr[minIdx]
+    arr[minIdx] = temp
   }
-  return sortedArray
 }
